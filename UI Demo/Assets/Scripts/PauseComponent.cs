4238f4fcdf6d3e005b9e1ec40df6dc3f3df319b5
ICS 161 <eaho1@uci.edu>
2019-01-29 20:32:17 -0800

    Added PauseMenu

--- UI Demo/Assets/Scripts/PauseComponent.cs
+++ UI Demo/Assets/Scripts/PauseComponent.cs
@@ -5,14 +5,23 @@ using TMPro;
 
 public class PauseComponent : MonoBehaviour
 {
+    public GameObject pausePanel;
     bool isPaused = false;
-    TextMeshProUGUI m_meshProUGUI;
+    //TextMeshProUGUI m_meshProUGUI;
 
     void Start()
     {
         //m_meshProUGUI = this.GetComponent<TextMeshProUGUI>();
         //m_meshProUGUI = this.transform.Find("PauseButtonText").GetComponent<TextMeshProUGUI>();
-        m_meshProUGUI = this.GetComponentInChildren<TextMeshProUGUI>();
+        //m_meshProUGUI = this.GetComponentInChildren<TextMeshProUGUI>(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
     }
 
     public void TogglePause()
@@ -22,12 +31,14 @@ public class PauseComponent : MonoBehaviour
         if (isPaused)
         {
             Time.timeScale = 0;
-            m_meshProUGUI.text = "Resume";
+            //m_meshProUGUI.text = "Resume";
+            pausePanel.SetActive(true);
         }
         else
         {
             Time.timeScale = 1;
-            m_meshProUGUI.text = "Pause";
+            //m_meshProUGUI.text = "Pause";
+            pausePanel.SetActive(false);
         }
         //Time.timeScale = isPaused ? 0 : 1;
     }
